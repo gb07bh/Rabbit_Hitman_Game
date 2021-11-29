2f1b2e1a957d19b9323c71f390619b43461f8fe9
Gaurav Bhanot <71587244+gb07bh@users.noreply.github.com>
2021-11-29 14:43:02 +0530

    Add files via upload

---
+++ congif.py
@@ -0,0 +1,22 @@
+import os
+
+FPS = 100
+screenSize = (640,480)
+Images = {
+    'rabbit':os.path.join(os.getcwd(),'resources/images/dude.png'),
+    'grass':os.path.join(os.getcwd(),'resources/images/grass.png'),
+    'castle':os.path.join(os.getcwd(),'resources/images/castle.png'),
+    'arrow':os.path.join(os.getcwd(),'resources/images/bullet.png'),
+    'badguy':os.path.join(os.getcwd(),'resources/images/badguy.png'),
+    'healthbar':os.path.join(os.getcwd(),'resources/images/healthbar.png'),
+    'health':os.path.join(os.getcwd(),'resources/images/health.png'),
+    'gameover':os.path.join(os.getcwd(),'resources/images/gameover.png'),
+    'youwin':os.path.join(os.getcwd(),'resources/images/youwin.png')
+}
+
+Sounds = {
+    'hit':os.path.join(os.getcwd(),'resources/audio/explode.wav'),
+    'enemy':os.path.join(os.getcwd(),'resources/audio/enemy.wav'),
+    'shoot':os.path.join(os.getcwd(),'resources/audio/shoot.wav'),
+    'moonlight':os.path.join(os.getcwd(),'resources/audio/moonlight.wav')
+}
